c0663e5b2037fe5b8f82b63d7b8287959cc69dd9
seungjo <f1v3@kakao.com>
2024-10-07 15:09:12 +0900

    test: 동시성 문제 발생

--- src/test/java/com/f1v3/coupon/service/ApplyServiceTest.java
+++ src/test/java/com/f1v3/coupon/service/ApplyServiceTest.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * {class name}.
@@ -32,4 +36,29 @@ class ApplyServiceTest {
         assertEquals(1, count);
     }
 
+    @Test
+    @DisplayName("여러명이 동시에 쿠폰 발급 요청")
+    void test2() throws InterruptedException {
+
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+
+        long count = couponRepository.count();
+        assertEquals(100, count);
+    }
 }
