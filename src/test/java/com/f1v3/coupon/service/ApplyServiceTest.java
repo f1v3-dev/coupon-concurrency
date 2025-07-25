202e6d0542d7590f114fb4766d219494994c1863
seungjo <f1v3@kakao.com>
2025-07-25 15:48:15 +0900

    test: 동시에 100회 이상 쿠폰 발급 테스트 (실패)

--- src/test/java/com/f1v3/coupon/service/ApplyServiceTest.java
+++ src/test/java/com/f1v3/coupon/service/ApplyServiceTest.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * {class name}.
- *
- * @author Seungjo, Jeong
- */
 @SpringBootTest
 class ApplyServiceTest {
 
@@ -28,5 +27,31 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertEquals(1, count);
+    }
+
+    @Test
+    void 쿠폰_여러명_발급() throws InterruptedException {
+        int threadCount = 1000;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+
+            executorService.execute(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+        assertEquals(100, count);
     }
 }
